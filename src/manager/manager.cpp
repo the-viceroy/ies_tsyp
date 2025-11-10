31879487547a85b4d2029e3b90c975fc15c30314
Viceroy <ayoub.ouali@egg.tn>
2025-11-10 21:14:13 +0100

    docs: add comment to do comment in manager

--- src/manager/manager.cpp
+++ src/manager/manager.cpp
@@ -1,6 +1,8 @@
 #include "manager.h"
 
 void Manager::poll() {
+  // Sleep call should be added to lessen power consumption.
+
   for (int i = 0; i < this->modules.size(); i++) {
     if (modules[i].condition(controller)) {
       modules[i].poll(controller);
